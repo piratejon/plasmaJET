4c150d80cb486c9ffbc4f9ee4bf48564640bcac2
Jonathan Wesley Stone <js@piratejon.com>
2015-03-18 23:54:57 -0500

    string increment function properly wraps

--- C/tests/tests.c
+++ C/tests/tests.c
@@ -17,10 +17,48 @@ void test_increment_position(void) {
 
 void test_increment_string(void) {
   char a[10] = "         ";
-  ttt_string_increment(a, sizeof(a));
+  ASSERT(sizeof(a) == 10, "bad sizeof");
+  ttt_string_increment(a, sizeof(a)-1);
   ASSERT(0 == strcmp(a, "x        "), "string failed to increment");
-  ttt_string_increment(a, sizeof(a));
+  ttt_string_increment(a, sizeof(a)-1);
   ASSERT(0 == strcmp(a, "o        "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, " x       "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "xx       "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "ox       "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, " o       "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "xo       "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "oo       "), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "  x      "), "string failed to increment");
+
+  strcpy(a, "oooooooox");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "        o"), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "x       o"), "string failed to increment");
+
+  strcpy(a, " oooooooo");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "xoooooooo"), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(0 == strcmp(a, "ooooooooo"), "string failed to increment");
+  ttt_string_increment(a, sizeof(a)-1);
+  ASSERT(' ' == a[0], "bad increment");
+  ASSERT(' ' == a[1], "bad increment");
+  ASSERT(' ' == a[2], "bad increment");
+  ASSERT(' ' == a[3], "bad increment");
+  ASSERT(' ' == a[4], "bad increment");
+  ASSERT(' ' == a[5], "bad increment");
+  ASSERT(' ' == a[6], "bad increment");
+  ASSERT(' ' == a[7], "bad increment");
+  ASSERT(' ' == a[8], "bad increment");
+  ASSERT(0 == strcmp(a, "         "), "string failed to increment");
 }
 
 void do_tests(void) {
