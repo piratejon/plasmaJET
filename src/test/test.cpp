cb2b833b1d6e915c2ac1d6ea88a9ab3fbefa7c73
Jonathan Wesley Stone <js@piratejon.com>
2015-03-22 14:58:25 -0500

    test TttBoard constructor/initializer

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -13,7 +13,10 @@ TEST_F(TttTest, ValidateTestFramework) {
   ASSERT_EQ(0, 0);
 }
 
-
+TEST_F(TttTest, TttBoardConstructor) {
+  TttBoard b = TttBoard("xx oo xo ");
+  ASSERT_EQ(b.getSpace(0), 'x');
+}
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
