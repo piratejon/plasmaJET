a0c85192c9aa3da046f3daaed366538f99967983
Jonathan Wesley Stone <js@piratejon.com>
2015-03-22 10:19:45 -0500

    starting up in C++

---
+++ CPP/src/TttBoard.h
@@ -0,0 +1,12 @@
+
+#if ! defined (PLASMAJET_TTTBOARD_H)
+#define PLASMAJET_TTTBOARD_H
+
+class TttBoard {
+  public:
+    TttBoard(const char *);
+    char at(int);
+};
+
+#endif // PLASMAJET_TTTBOARD_H
+
---
+++ CPP/test/test.cpp
@@ -0,0 +1,20 @@
+
+#include <gtest/gtest.h>
+
+#include "TttBoard.h"
+
+namespace {
+  class TttTest : public ::testing::Test {
+  };
+}
+
+TEST_F(TttTest, BoardFromString) {
+  TttBoard t = TttBoard("xx oo xox");
+  ASSERT_EQ(t.at(0), 'x');
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
